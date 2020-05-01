a6073873e1c6791a2d99dc0262c49f7656332e50
Brian Pollack <brianleepollack@gmail.com>
2020-05-01 11:19:12 -0400

    updates to stratification (multi and single)

--- mre/train_mre_model.py
+++ mre/train_mre_model.py
@@ -17,6 +17,7 @@ from torch.utils.data.sampler import RandomSampler
 from torchsummary import summary
 from tensorboardX import SummaryWriter
 from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit
 
 from mre.mre_datasets import MREtoXr, MRETorchDataset
 from mre.prediction import train_model, add_predictions, add_val_linear_cor
@@ -159,9 +160,10 @@ def train_model_full(data_path: str, data_file: str, output_path: str, model_ver
         df_strat = pd.read_pickle(
             '/pghbio/dbmi/batmanlab/bpollack/predictElasticity/data/MRE/df_strat_v0.pkl')
         df_strat = df_strat.drop(index=test_list)
-        mskf = MultilabelStratifiedShuffleSplit(n_splits=1, test_size=30)
-        splits = mskf.split(df_strat.index.values, df_strat[['gender', 'mre_group', 'bmi_group',
-                                                             'age_group']].values)
+        # mskf = MultilabelStratifiedShuffleSplit(n_splits=1, test_size=30,
+        #                                         random_state=cfg['seed'])
+        mskf = StratifiedShuffleSplit(n_splits=1, test_size=30, random_state=cfg['seed'])
+        splits = mskf.split(df_strat.index.values, df_strat[['mre_group']].values.flatten())
         train_index, val_index = next(splits)
         val_list = list(df_strat.index.values[val_index])
         train_list = list(df_strat.index.values[train_index])
